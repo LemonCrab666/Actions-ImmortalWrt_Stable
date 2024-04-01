9c4097649d83bdc4c34fcf6808056434a4b9a8c9
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-04-01 21:46:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,13 +18,8 @@
 
 # 生成vermagic文件
 echo "317eb6a6d9828371f8f0ca9cfaff251a" > vermagic
-
-# 修改include/kernel-defaults.mk文件
-sed -i "s|grep '=[ym]' \$(LINUX_DIR)/.config.set \| LC_ALL=C sort \| \$(MKHASH) md5 > \$(LINUX_DIR)/.vermagic|cp \$(TOPDIR)/vermagic \$(LINUX_DIR)/.vermagic|g" include/kernel-defaults.mk
-
-# 修改package/kernel/linux/Makefile文件
-sed -i "s|STAMP_BUILT:=\$(STAMP_BUILT)_\$(shell \$(SCRIPT_DIR)/kconfig.pl \$(LINUX_DIR)/.config \| \$(MKHASH) md5)|STAMP_BUILT:=\$(STAMP_BUILT)_\$(shell cat \$(LINUX_DIR)/.vermagic)|g" package/kernel/linux/Makefile
-
+sed -i "s/grep '=[ym]' \$(LINUX_DIR)/.config.set \| LC_ALL=C sort \| \$(MKHASH) md5 > \$(LINUX_DIR)/.vermagic/cp \$(TOPDIR)/vermagic \$(LINUX_DIR)/.vermagic/g" include/kernel-defaults.mk
+sed -i "s/\(STAMP_BUILT:=\)\(.*\)\(_\$\(shell \)\$(SCRIPT_DIR)/kconfig.pl \$(LINUX_DIR)/.config \| \$(MKHASH) md5\)\)/\1\2_\$\(shell cat \$(LINUX_DIR)/.vermagic\)\3/g" package/kernel/linux/Makefile
 
 #添加软件包
 #rm -rf feeds/luci/applications/luci-app-openclash
