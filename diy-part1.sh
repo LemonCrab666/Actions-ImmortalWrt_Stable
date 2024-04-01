e9dfa8380f08ade0fafa2b7aec83a3b605cfe153
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-04-01 12:55:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,3 @@
 
 # 添加feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
