b46685e54da294576710e76772f8de4c1f1d1631
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-04-02 09:30:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,16 +17,3 @@
 #rm -rf feeds/luci/applications/luci-app-openclash
 #git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-
-#!/bin/bash
-
-# 生成vermagic文件
-echo "317eb6a6d9828371f8f0ca9cfaff251a" > vermagic
-
-# 修改include/kernel-defaults.mk文件
-sed -i '/grep '=[ym]' $(LINUX_DIR)\/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/.vermagic/s/^/#/' /workdir/openwrt/include/kernel-defaults.mk
-sed -i '/grep '=[ym]' $(LINUX_DIR)\/.config.set | LC_ALL=C sort | $(MKHASH) md5 > $(LINUX_DIR)\/.vermagic/a\cp $(TOPDIR)\/vermagic $(LINUX_DIR)\/.vermagic' /workdir/openwrt/include/kernel-defaults.mk
-
-# 修改package/kernel/linux/Makefile文件
-sed -i 's/STAMP_BUILT:=$(STAMP_BUILT)_$(shell $(SCRIPT_DIR)\/kconfig.pl $(LINUX_DIR)\/.config | $(MKHASH) md5)/# &/' /workdir/openwrt/package/kernel/linux/Makefile
-sed -i '/STAMP_BUILT:=$(STAMP_BUILT)_$(shell $(SCRIPT_DIR)\/kconfig.pl $(LINUX_DIR)\/.config | $(MKHASH) md5)/a\STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)\/.vermagic)' /workdir/openwrt/package/kernel/linux/Makefile
