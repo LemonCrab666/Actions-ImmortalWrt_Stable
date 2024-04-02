9e37d23139d309e328faddd5684c61edd2c7a77a
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-04-02 11:02:00 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -21,6 +21,12 @@ git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app
 # 修改固件MD5值
 # 生成VerMagic文件
 echo "317eb6a6d9828371f8f0ca9cfaff251a" > vermagic
+# 检查VerMagic文件是否生成成功
+if [ ! -f "vermagic" ]; then
+    echo "VerMagic文件生成失败！"
+    exit 1
+fi
+
 # 修改include/kernel-defaults.mk
 # 设置变量
 pattern="grep '=[ym]' \$(LINUX_DIR)/.config.set | LC_ALL=C sort | \$(MKHASH) md5 > \$(LINUX_DIR)/.vermagic"
@@ -29,5 +35,20 @@ replacement="cp \$(TOPDIR)/vermagic \$(LINUX_DIR)/.vermagic"
 escaped_pattern=$(printf '%s\n' "$pattern" | sed -e 's/[][\/$*.^|[]/\\&/g')
 # 使用sed命令替换整段语句
 sed -i "s|$escaped_pattern|$replacement|g" include/kernel-defaults.mk
+# 检查替换是否成功
+if [ $? -ne 0 ]; then
+    echo "include/kernel-defaults.mk 替换失败！"
+    exit 1
+fi
+
 # 修改package/kernel/linux/Makefile
 sed -i 's/STAMP_BUILT:=$(STAMP_BUILT)_$(shell $(SCRIPT_DIR)\/kconfig.pl $(LINUX_DIR)\/.config | $(MKHASH) md5)/STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)\/.vermagic)/g' package/kernel/linux/Makefile
+# 检查替换是否成功
+if [ $? -ne 0 ]; then
+    echo "package/kernel/linux/Makefile 替换失败！"
+    exit 1
+fi
+
+# 输出成功信息
+echo "替换成功！"
+
