3246bed43ece1d6d289a6abe94d55605e7c5f2db
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-10-10 23:06:45 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,56 +27,6 @@ mv OpenWrt-mihomo/mihomo package/
 git clone https://github.com/hudra0/qosmate.git package/qosmate
 git clone https://github.com/hudra0/luci-app-qosmate.git package/luci-app-qosmate
 
-#首次启动运行脚本
-mkdir -p files/etc/uci-defaults
-
-cat << "EOF" > files/etc/uci-defaults/70-rootpt-resize
-if [ ! -e /etc/rootpt-resize ] \
-&& type parted > /dev/null \
-&& lock -n /var/lock/root-resize
-then
-ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
-'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
-ROOT_DISK="/dev/$(basename "${ROOT_BLK%/*}")"
-ROOT_PART="${ROOT_BLK##*[^0-9]}"
-parted -f -s "${ROOT_DISK}" \
-resizepart "${ROOT_PART}" 100%
-mount_root done
-touch /etc/rootpt-resize
-reboot
-fi
-exit 1
-EOF
-
-chmod 0644 files/etc/uci-defaults/70-rootpt-resize
-
-cat << "EOF" > files/etc/uci-defaults/80-rootfs-resize
-if [ ! -e /etc/rootfs-resize ] \
-&& [ -e /etc/rootpt-resize ] \
-&& type losetup > /dev/null \
-&& type resize2fs > /dev/null \
-&& lock -n /var/lock/root-resize
-then
-ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
-'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
-ROOT_DEV="/dev/${ROOT_BLK##*/}"
-LOOP_DEV="$(awk -e '$5=="/overlay"{print $9}' \
-/proc/self/mountinfo)"
-if [ -z "${LOOP_DEV}" ]
-then
-LOOP_DEV="$(losetup -f)"
-losetup "${LOOP_DEV}" "${ROOT_DEV}"
-fi
-resize2fs -f "${LOOP_DEV}"
-mount_root done
-touch /etc/rootfs-resize
-reboot
-fi
-exit 1
-EOF
-
-chmod 0644 files/etc/uci-defaults/80-rootfs-resize
-
 #修改sysguarde备份列表
 mkdir -p files/etc
 
@@ -93,8 +43,6 @@ cat <<EOF > files/etc/sysupgrade.conf
 /usr/share/wechatpush/api/OpenWrt.jpg
 /root/backup_openwrt.sh
 /root/sshpass
-/etc/uci-defaults/70-rootpt-resize
-/etc/uci-defaults/80-rootfs-resize
 EOF
 
 chmod 0644 files/etc/sysupgrade.conf
