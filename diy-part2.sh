7fa66c310cfdeeba9780acf534ea12c247b48f97
LemonCrab <55867630+LemonCrab666@users.noreply.github.com>
2024-10-10 14:21:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -38,12 +38,12 @@ if [ ! -e /etc/rootpt-resize ] \
 && type parted > /dev/null \
 && lock -n /var/lock/root-resize
 then
-ROOT_BLK="\$(readlink -f /sys/dev/block/\"\$(awk -e \
-'\$9==\"/dev/root\"{print \$3}' /proc/self/mountinfo)\")"
-ROOT_DISK="/dev/\$(basename "\${ROOT_BLK%/*}")"
-ROOT_PART="\${ROOT_BLK##*[^0-9]}"
-parted -f -s "\${ROOT_DISK}" \
-resizepart "\${ROOT_PART}" 100%
+ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
+'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
+ROOT_DISK="/dev/$(basename "${ROOT_BLK%/*}")"
+ROOT_PART="${ROOT_BLK##*[^0-9]}"
+parted -f -s "${ROOT_DISK}" \
+resizepart "${ROOT_PART}" 100%
 mount_root done
 touch /etc/rootpt-resize
 reboot
@@ -58,28 +58,23 @@ if [ ! -e /etc/rootfs-resize ] \
 && type resize2fs > /dev/null \
 && lock -n /var/lock/root-resize
 then
-ROOT_BLK="\$(readlink -f /sys/dev/block/\"\$(awk -e \
-'\$9==\"/dev/root\"{print \$3}' /proc/self/mountinfo)\")"
-ROOT_DEV="/dev/\${ROOT_BLK##*/}"
-LOOP_DEV="\$(awk -e '\$5==\"/overlay\"{print \$9}' \
+ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
+'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
+ROOT_DEV="/dev/${ROOT_BLK##*/}"
+LOOP_DEV="$(awk -e '$5=="/overlay"{print $9}' \
 /proc/self/mountinfo)"
-if [ -z "\${LOOP_DEV}" ]
+if [ -z "${LOOP_DEV}" ]
 then
-LOOP_DEV="\$(losetup -f)"
-losetup "\${LOOP_DEV}" "\${ROOT_DEV}"
+LOOP_DEV="$(losetup -f)"
+losetup "${LOOP_DEV}" "${ROOT_DEV}"
 fi
-resize2fs -f "\${LOOP_DEV}"
+resize2fs -f "${LOOP_DEV}"
 mount_root done
 touch /etc/rootfs-resize
 reboot
 fi
 exit 1
 EOF2
-
-cat << "EOF3" >> /etc/sysupgrade.conf
-/etc/uci-defaults/70-rootpt-resize
-/etc/uci-defaults/80-rootfs-resize
-EOF3
 
 sh /etc/uci-defaults/70-rootpt-resize
 
